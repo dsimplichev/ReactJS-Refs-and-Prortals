2ade7c1bc75fa33304d4c97969e8db0bf74e97d7
dsimplichev <dsimplichev1948@gmail.com>
2024-02-15 18:02:59 +0000

    add new components timer challenge

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,7 +4,7 @@ import { useState, useRef } from "react";
 export default function Player() {
   const playerName = useRef();
 
-  const [enteredPlayerName, setEnteredPlayerName] = useState('');
+ const [enteredPlayerName, setEnteredPlayerName] = useState('');
 
   
 function handleChange(event) {
@@ -13,13 +13,13 @@ function handleChange(event) {
 }
 
 function handleClick() {
- setEnteredPlayerName(playerName.current.value);
- playerName.current.value = '';
+setEnteredPlayerName(playerName.current.value);
+playerName.current.value = '';
 }
 
   return (
     <section id="player">
-      <h2>Welcome {enteredPlayerName ??'unknown entity'} </h2>
+      <h2>Welcome {enteredPlayerName ?? 'unknown entity'} </h2>
       <p>
         <input ref={playerName} type="text" />
         <button onClick={handleClick}>Set Name</button>
