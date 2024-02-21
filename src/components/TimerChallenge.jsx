7c2d2c28df4f9ade29eb630a4858e262d0857504
dsimplichev <dsimplichev1948@gmail.com>
2024-02-21 09:24:33 +0000

    seting timer using useState

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,12 +1,22 @@
 export default function TimerChallenge({ title, targetTime }) {
-     return(
+   const[timerStarted, setTimerStarted] = useState(false);
+  const  [timeExpired, setTimerExpired] = useState(false); 
+  function handleStart() {
+    setTimeout(() => {
+      setTimerExpired(true);
+    },targetTime * 1000);
+   setTimerStarted(true)
+  }
+  
+  return (
         <section className="challenge">
           <h2>{title}</h2>
+          {timeExpired && <p>You lost!</p>}
           <p className="challenge-time">
             {targetTime} second{targetTime > 1 ? 's' : ''}
           </p>
           <p>
-            <button>
+            <button onClick={handleStart}>
                 Start Challenge
             </button>
           </p>
